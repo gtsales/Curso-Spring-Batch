b22478e9af1ed8d2c94d8334a7449f42a742fb33
Luiz Sales <soutoluiz0609@gmail.com>
2024-10-11 17:45:11 -0300

    first spring batch project

---
+++ firstSpringBatchProject/src/main/java/luiz/sales/springbatch/FirstSpringBatchProjectApplication.java
@@ -0,0 +1,13 @@
+package luiz.sales.springbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSpringBatchProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstSpringBatchProjectApplication.class, args);
+	}
+
+}
